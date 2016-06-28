240ca13e1a2150feda57000be6d7bfbaabe90eb1
Samarth Vadia <samarth.vadia12@gmail.com>
2016-06-28 13:04:37 -0400

    Add project files

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DatabaseHelper.DatabaseHelper dbhelper = new DatabaseHelper.DatabaseHelper("18.62.9.117", "18.62.9.117", "root", "w0lfg4ng", "BECIVDatabase");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            string[] c = dbhelper.getLastNImageTimestamps(2);
+            int b = 2;
+        }
+    }
+}
